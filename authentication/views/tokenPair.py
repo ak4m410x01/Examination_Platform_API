6dcc10b8731ac735c60392009c87ba48d5324932
ak4m410x01 <ak4m410x01@gmail.com>
2024-02-27 20:13:56 +0200

    set allow any permissions for obtain token endpoint

--- authentication/views/tokenPair.py
+++ authentication/views/tokenPair.py
@@ -1,6 +1,8 @@
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from authentication.serializers.tokenPair import TokenPairSerializer
 
 
 class TokenObtainPair(TokenObtainPairView):
     serializer_class = TokenPairSerializer
+    permission_classes = (AllowAny,)
