2c203c3b407ed867ec7664e3d3af1959c28e5074
ak4m410x01 <ak4m410x01@gmail.com>
2024-02-27 10:36:54 +0200

    add actors to admin panel

--- accounts/admin/__init__.py
+++ accounts/admin/__init__.py
@@ -1,1 +1,3 @@
 from accounts.admin.admin import Admin
+from accounts.admin.instructor import Instructor
+from accounts.admin.student import Student
---
+++ accounts/admin/student.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from accounts.models.student import Student
+
+admin.site.register(Student)
