044312d9e7f767715b8b3cc06500dacacb9a2bfd
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2024-02-29 03:23:43 +0200

    Add admin views and models for course student enrollment, courses, levels, and department

---
+++ levels/admin/__init__.py
@@ -0,0 +1,4 @@
+from levels.admin.course_student_enrollment import CourseStudentEnrollment
+from levels.admin.courses import Course
+from levels.admin.levels import Level
+from levels.admin.department import Department
---
+++ levels/admin/courses.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from levels.models.courses import Course
+
+admin.site.register(Course)
