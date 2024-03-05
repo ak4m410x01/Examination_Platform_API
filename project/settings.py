5f10876aa85e66c341df2fed1917749935251999
ak4m410x01 <ak4m410x01@gmail.com>
2024-03-05 15:32:49 +0200

    Change JSON Rendering & Parsing in response

--- project/settings.py
+++ project/settings.py
@@ -92,8 +92,8 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     # JSON Renderer => uncomment in Production environment.
-    # "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
-    # "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
+    "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
+    "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
     # Authentication
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
