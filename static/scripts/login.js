a917778b9a7d4f672106e0bc04090f2d4af12ac3
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-05-18 08:06:03 +0300

    Implement AJAX-based User Login Functionality
    This JavaScript script is used for handling user login in a web application. It uses jQuery to listen for the form submission event. When the form is submitted, it prevents the default form submission behavior and makes an AJAX POST request to the '/api/auth/token/' endpoint with the username and password entered by the user. If the request is successful, the user is redirected to the dashboard. If the request fails, an error message is displayed to the user.

---
+++ static/scripts/login.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $('form').on('submit', function (event) {
+    // Prevent the form from submitting the traditional way
+    event.preventDefault();
+    $('#flash-message').text('Incorrect username or password.').hide();
+
+    const username = $('#username').val();
+    const password = $('#pass').val();
+
+    $.ajax({
+      url: 'http://localhost:5000/api/auth/token/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        username: username,
+        password: password
+      }),
+      success: function () {
+        window.location.href = 'http://localhost/dashboard';
+      },
+      error: function () {
+        $('#flash-message').text('Incorrect username or password.').show();
+      }
+    });
+  });
+});
