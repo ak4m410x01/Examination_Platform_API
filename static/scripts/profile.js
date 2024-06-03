b2f9eed2d6dfc65dce3726d99bbd27952ddf0bac
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-06-03 15:58:34 +0300

    Update page titles and scripts for profile.html and index.html

--- static/scripts/profile.js
+++ static/scripts/profile.js
@@ -1,19 +1,20 @@
-// on updating data
-$("#update").click(function () {
-    $("input").each(function (el) {
-        
-            $(this).removeAttr('readonly');
-            
-        // console.log("abeer");
-    })
-})
+$(document).ready(function () {
+  // on updating data
+  $('#update').click(function () {
+    $('input').each(function (el) {
+      $(this).removeAttr('readonly');
+      // console.log("abeer");
+    });
+    $(this).css('background-color', 'white'); // Change the button color to green
+  });
 
-// on saving data 
-$("#save").click(function () {
-    $("input").each(function (el) {
-
-        $(this).attr('readonly', 'true');
-            
-        // console.log("abeer");
-    })
-})
+  // on saving data
+  $('#save').click(function () {
+    $('input').each(function (el) {
+      $(this).attr('readonly', 'true');
+      // console.log("abeer");
+    });
+    $('#update').css('background-color', '#b3d6f5'); // Change the button color to green
+  })
+  ;
+});
