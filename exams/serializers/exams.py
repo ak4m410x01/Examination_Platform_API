e8a7ee0893d70178dfe85ab2502388efae944786
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-06-09 01:34:46 +0300

    Refactor ExamSerializer to convert start_date and end_date to Cairo timezone

--- exams/serializers/exams.py
+++ exams/serializers/exams.py
@@ -69,3 +69,16 @@ class ExamSerializer(serializers.ModelSerializer):
             end_date_tz = end_date_utc.astimezone(cairo_tz)
             validated_data['end_date'] = end_date_tz
         return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        # Convert start_date and end_date to Cairo timezone before saving
+        cairo_tz = pytz_timezone('Africa/Cairo')
+        if 'start_date' in validated_data:
+            start_date_utc = validated_data['start_date']
+            start_date_tz = start_date_utc.astimezone(cairo_tz)
+            validated_data['start_date'] = start_date_tz
+        if 'end_date' in validated_data:
+            end_date_utc = validated_data['end_date']
+            end_date_tz = end_date_utc.astimezone(cairo_tz)
+            validated_data['end_date'] = end_date_tz
+        return super().update(instance, validated_data)
